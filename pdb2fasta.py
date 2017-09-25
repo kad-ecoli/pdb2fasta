200e7ddee98e4ea38b379f9bdfee296c0d9168ae
Chengxin Zhang <zcx@dcmb-yzl09.med.umich.edu>
2017-09-25 15:40:31 -0400

    fix bug in chain order

--- pdb2fasta.py
+++ pdb2fasta.py
@@ -19,6 +19,7 @@ ca_pattern=re.compile("^ATOM\s{2,6}\d{1,5}\s{2}CA\s[\sA]([A-Z]{3})\s([\s\w])|^HE
 for pdb_file in sys.argv[1:]:
     filename=os.path.basename(pdb_file).split('.')[0]
     chain_dict=dict()
+    chain_list=[]
 
     fp=open(pdb_file,'rU')
     for line in fp.read().splitlines():
@@ -32,7 +33,8 @@ for pdb_file in sys.argv[1:]:
                 chain_dict[chain]+=aa3to1[resn]
             else:
                 chain_dict[chain]=aa3to1[resn]
+                chain_list.append(chain)
     fp.close()
 
-    for chain in sorted(chain_dict):
+    for chain in chain_list:
         sys.stdout.write('>%s:%s\n%s\n'%(filename,chain,chain_dict[chain]))
